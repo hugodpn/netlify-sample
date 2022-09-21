f31b3c6c6ef58b43bb66750a47d4f80f6a86e2d2
Hugo Pacini <hugo.pacini@halopowered.com>
2022-09-21 13:03:31 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    unloadEvent();
+  }, []);
+
+  function scanReceived(params:any){
+      // No data or no timestamp, scan failed.
+      if(params['data']== "" || params['time']==""){
+          //@ts-ignore
+          document.getElementById('display').innerHTML = "Failed!";
+          return;
+      }
+      // Data and timestamp exist, barcode successful, show results
+      var displayStr = "Barcode Data: " + params['data']+"<br>Time: "+params['time'];
+      //@ts-ignore
+      document.getElementById("display").innerHTML = displayStr;
+  }
+
+  function enableScanners(){
+      console.info("-------> enabling....");
+      //@ts-ignore
+      EB.Barcode.enable({}, scanReceived);
+      // Empty property hash, '{}' loads default values for the scanner.
+  }
+
+  function unloadEvent(){
+      //@ts-ignore
+      EB.Barcode.disable();
+      // Disable Barcode on unload of page to free it up for other operations.
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Barcode API Test</h1>
+      <div id="display">
+          Barcode Data: <br />
+          Time: <br />
+      </div>
+      <button onClick={enableScanners}>Enable Barcode Scanners</button>
     </div>
   );
 }
